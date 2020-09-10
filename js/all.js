9cedbe3db261857a1874e0723c58e71ee9b0e405
qw753954 <68770475+qw753954@users.noreply.github.com>
2020-09-10 19:36:12 +0800

    Modify

--- js/all.js
+++ js/all.js
@@ -5,32 +5,3 @@ AOS.init({
 $('.hamIcon').click(function() {
     $('.navbar').toggleClass('ham-active');
 })
-
-$('.product-item:first-child i').click(function() {
-    $(this).toggleClass('heartIng');
-})
-
-$('.product-item:nth-child(2) i').click(function() {
-    $(this).toggleClass('heartIng');
-
-})
-
-$('.product-item:nth-child(3) i').click(function() {
-    $(this).toggleClass('heartIng');
-
-})
-
-$('.product-item:nth-child(4) i').click(function() {
-    $(this).toggleClass('heartIng');
-
-})
-
-$('.product-item:nth-child(5) i').click(function() {
-    $(this).toggleClass('heartIng');
-
-})
-
-$('.product-item:last-child i').click(function() {
-    $(this).toggleClass('heartIng');
-
-})
